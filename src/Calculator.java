d9cf2258503f84bccab18636a501168cd8e07d45
mahesh <gohilmaheshhh@gmail.com>
2022-10-30 14:34:38 +0000

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    static int a =10;
+        static int b =5;
+        public static void add()
+        {
+            System.out.println(a+b);
+        }
+
+        public static void main(String[] args) {
+            add();
+        }
+    }
+
+
